f34237078a1652bd29ba352e3f20871666fb349c
zjs901 <zakjs901@gmail.com>
2025-03-18 11:39:00 +0000

    ADDED FILE READER

---
+++ Algorithms Assignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Application 
+{
+    
+    class main 
+    {
+        static void Main(string[] args) 
+        {
+            //Search();
+            DataCompile();
+        }
+
+        static void DataCompile()
+        {
+            string data;
+            StreamReader reader = null;
+            try
+            {
+                reader = new StreamReader("Share_1_data.txt");
+                data = reader.ReadLine();
+
+                while (data != null)
+                {
+                    Console.WriteLine(data);
+                    data = reader.ReadLine();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                reader.Close();
+            }
+        }
+
+        static void Search(int[] array)
+        {
+            Console.WriteLine("Searching...");
+        }
+    }
+}
